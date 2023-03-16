a9f0dfa205849b7f8a285234a6944c9f31f5433a
Tim Andersson <Tim.1996@live.se>
2023-03-16 19:31:56 +0100

    "changed route "

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,10 +1,35 @@
-import React from 'react';
-import { Navigate } from 'react-router';
-import Cookies from 'js-cookie';
+import React, { useState, useEffect } from 'react';
+import { Navigate } from 'react-router-dom';
 
 const PrivateRoute = ({ children }) => {
-  const jwtToken = Cookies.get('JWT_TOKEN');
-  return jwtToken ? children : <Navigate to="/" />;
+  const [isLoading, setIsLoading] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const checkAuthentication = async () => {
+      try {
+        const response = await fetch('http://localhost:8080/api/v1/auth/authenticated', {
+          method: 'GET',
+          credentials: 'include',
+        });
+
+        const isAuthenticated = await response.json();
+        setIsAuthenticated(isAuthenticated);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    checkAuthentication();
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return isAuthenticated ? children : <Navigate to="/" />;
 };
 
 export default PrivateRoute;
