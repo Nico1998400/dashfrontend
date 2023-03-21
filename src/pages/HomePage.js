6022c4f1cb93473ff645e1d5bac34de6e0b0df5b
Tim Andersson <Tim.1996@live.se>
2023-03-21 17:10:23 +0100

    Super fetch in homie

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './HomePage.css';
 
 const HomePage = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [categories, setCategories] = useState([]);
 
-export default HomePage
+  const fetchCategories = async () => {
+   
+    try {
+      const response = await fetch('http://localhost:8080/api/v1/category', {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+      
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`Failed to fetch categories: ${response.statusText}`);
+      }
+
+      const fetchedCategories = await response.json();
+      setCategories(fetchedCategories);
+    } catch (error) {
+      console.error('Error fetching categories:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCategories();
+    console.log(categories);
+  }, []);
+  const handleCategoryChange = (event) => {
+    console.log('Retter', event.target.value);
+  };
+
+  return (
+    <div>
+    <h1>Home</h1>
+    <select onChange={handleCategoryChange}>
+    <option value="" >Select a category</option>
+      {categories.map((category) => (
+        <option key={category.id} value={category.id}>
+          {category.categoryTitle}
+        </option>
+      ))}
+    </select>
+  </div>
+);
+};
+
+export default HomePage;
