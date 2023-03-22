f1e820173091473b3a25973bcd4d0c6b3da96d8f
Tim Andersson <Tim.1996@live.se>
2023-03-22 12:34:07 +0100

    changed some parameters

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,9 +3,10 @@ import './HomePage.css';
 
 const HomePage = () => {
   const [categories, setCategories] = useState([]);
+  const [foodItems, setFoodItems] = useState([]);
+
 
   const fetchCategories = async () => {
-   
     try {
       const response = await fetch('http://localhost:8080/api/v1/category', {
         method: 'GET',
@@ -31,23 +32,41 @@ const HomePage = () => {
     fetchCategories();
     console.log(categories);
   }, []);
+
+
   const handleCategoryChange = (event) => {
-    console.log('Retter', event.target.value);
+    const categoryId = event.target.value;
+    if (categoryId) {
+      const selectedCategory = categories.find((category) => category.id === parseInt(categoryId));
+      setFoodItems(selectedCategory.foodItems);
+    } else {
+      setFoodItems([]);
+    }
   };
 
   return (
     <div>
-    <h1>Home</h1>
-    <select onChange={handleCategoryChange}>
-    <option value="" >Select a category</option>
-      {categories.map((category) => (
-        <option key={category.id} value={category.id}>
-          {category.categoryTitle}
-        </option>
-      ))}
-    </select>
-  </div>
-);
+      <h1>Home</h1>
+      <select onChange={handleCategoryChange}>
+        <option value="">Select a category</option>
+        {categories.map((category) => (
+          <option key={category.id} value={category.id}>
+            {category.categoryTitle}
+          </option>
+        ))}
+      </select>
+      <div className="food-items">
+        {foodItems.map((foodItem) => (
+          <div key={foodItem.id} className="food-item">
+            <h3>{foodItem.foodName}</h3>
+            <p>{foodItem.foodDescription}</p>
+            <p>Food Number: {foodItem.foodNumber}</p>
+            <p>Price: {foodItem.price}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
